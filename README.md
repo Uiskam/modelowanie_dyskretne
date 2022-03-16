f30fcfe4adf1f3e0bc2e29b91c47000a206f3bec
Uiskam <92760953+Uiskam@users.noreply.github.com>
2022-03-16 13:06:11 +0100

    w2tf

--- README.md
+++
@@ -1,2 +0,0 @@
-Maksymilian Sulima
-rozwiązanie zadania z lab1 znajduje się w Lab1/GameOfLife
---
+++ lab3-cars/src/Point.java
@@ -0,0 +1,16 @@
+public class Point {
+
+    // TODO
+
+    public void move() {
+        // TODO
+    }
+
+    public void clicked() {
+        // TODO
+    }
+
+    public void clear() {
+        // TODO
+    }
+}
