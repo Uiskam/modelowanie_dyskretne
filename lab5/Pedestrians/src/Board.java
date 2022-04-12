08141f408da7df659f789f3c425f6421f95c64b3
maks <you@example.com>
2022-04-13 00:54:44 +0200

    1/2 done

--- lab5/Pedestrians/src/Board.java
+++ lab5/Pedestrians/src/Board.java
@@ -10,160 +10,194 @@ import javax.swing.JComponent;
 import javax.swing.event.MouseInputListener;
 
 public class Board extends JComponent implements MouseInputListener, ComponentListener {
-	private static final long serialVersionUID = 1L;
-	private Point[][] points;
-	private int size = 10;
-	public int editType=0;
+    private static final long serialVersionUID = 1L;
+    private Point[][] points;
+    private int size = 10;
+    public int editType = 0;
 
-	public Board(int length, int height) {
-		addMouseListener(this);
-		addComponentListener(this);
-		addMouseMotionListener(this);
-		setBackground(Color.WHITE);
-		setOpaque(true);
-	}
+    public Board(int length, int height) {
+        addMouseListener(this);
+        addComponentListener(this);
+        addMouseMotionListener(this);
+        setBackground(Color.WHITE);
+        setOpaque(true);
+    }
 
-	public void iteration() {
-		for (int x = 1; x < points.length - 1; ++x)
-			for (int y = 1; y < points[x].length - 1; ++y)
-				points[x][y].move();
-		this.repaint();
-	}
+    public void iteration() {
+        for (int x = 1; x < points.length - 1; ++x)
+            for (int y = 1; y < points[x].length - 1; ++y)
+                points[x][y].move();
+        this.repaint();
+    }
 
-	public void clear() {
-		for (int x = 0; x < points.length; ++x)
-			for (int y = 0; y < points[x].length; ++y) {
-				points[x][y].clear();
-			}
-		calculateField();
-		this.repaint();
-	}
+    public void clear() {
+        for (int x = 0; x < points.length; ++x)
+            for (int y = 0; y < points[x].length; ++y) {
+                points[x][y].clear();
+            }
+        calculateField();
+        this.repaint();
+    }
 
-	private void initialize(int length, int height) {
-		points = new Point[length][height];
+    private void initialize(int length, int height) {
+        points = new Point[length][height];
 
-		for (int x = 0; x < points.length; ++x)
-			for (int y = 0; y < points[x].length; ++y)
-				points[x][y] = new Point();
+        for (int x = 0; x < points.length; ++x)
+            for (int y = 0; y < points[x].length; ++y)
+                points[x][y] = new Point();
 
-		for (int x = 1; x < points.length-1; ++x) {
-			for (int y = 1; y < points[x].length-1; ++y) {			
-			}
-		}	
-	}
-	
-	private void calculateField(){
-	}
+        for (int x = 1; x < points.length - 1; ++x) {
+            for (int y = 1; y < points[x].length - 1; ++y) {
+                boolean mooreNeiMode = false;
+                if (mooreNeiMode) {
+                    Point curPoint = points[x][y];
+                    int[] dX = {-1, -1, 0, 1, 1, 1, 0, -1};
+                    int[] dY = {0, 1, 1, 1, 0, -1, -1, -1};
+                    for (int i = 0; i < 8; i++) {
+                        curPoint.addNeighbor(points[(x + dX[i] + points.length) % points.length][(y + dY[i] + points[x].length) % points[x].length]);
+                    }
+                } else {
+                    Point curPoint = points[x][y];
+                    int[] dX = {-1, 0, 1, 0};
+                    int[] dY = {0, 1, 0, -1};
+                    for (int i = 0; i < dX.length; i++) {
+                        curPoint.addNeighbor(points[(x + dX[i] + points.length) % points.length][(y + dY[i] + points[x].length) % points[x].length]);
+                    }
+                }
+            }
+        }
+    }
 
-	protected void paintComponent(Graphics g) {
-		if (isOpaque()) {
-			g.setColor(getBackground());
-			g.fillRect(0, 0, this.getWidth(), this.getHeight());
-		}
-		g.setColor(Color.GRAY);
-		drawNetting(g, size);
-	}
+    private void initNeighborhood(int x, int y, boolean mooreNeiMode) {
 
-	private void drawNetting(Graphics g, int gridSpace) {
-		Insets insets = getInsets();
-		int firstX = insets.left;
-		int firstY = insets.top;
-		int lastX = this.getWidth() - insets.right;
-		int lastY = this.getHeight() - insets.bottom;
+    }
 
-		int x = firstX;
-		while (x < lastX) {
-			g.drawLine(x, firstY, x, lastY);
-			x += gridSpace;
-		}
+    private void calculateField() {
+        ArrayList<Point> toCheck = new ArrayList<>();
+        //System.out.println("exit INIT");
+        for (Point[] row : points) {
+            for (Point point : row) {
+                if (point.type == 2) {
+                    point.staticField = 0;
+                    toCheck.addAll(point.neighbors);
+                    //System.out.println("exit add3ed");
+                }
+            }
+        }
+        while (!toCheck.isEmpty()) {
+            //System.out.println(toCheck.size());
+            if(toCheck.get(0).calcStaticField()){
+                toCheck.addAll(toCheck.get(0).neighbors);
+            }
+            toCheck.remove(0);
+        }
+    }
 
-		int y = firstY;
-		while (y < lastY) {
-			g.drawLine(firstX, y, lastX, y);
-			y += gridSpace;
-		}
+    protected void paintComponent(Graphics g) {
+        if (isOpaque()) {
+            g.setColor(getBackground());
+            g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        }
+        g.setColor(Color.GRAY);
+        drawNetting(g, size);
+    }
 
-		for (x = 1; x < points.length-1; ++x) {
-			for (y = 1; y < points[x].length-1; ++y) {
-				if(points[x][y].type==0){
-					float staticField = points[x][y].staticField;
-					float intensity = staticField/100;
-					if (intensity > 1.0) {
-						intensity = 1.0f;
-					}
-					g.setColor(new Color(intensity, intensity,intensity ));
-				}
-				else if (points[x][y].type==1){
-					g.setColor(new Color(1.0f, 0.0f, 0.0f, 0.7f));
-				}
-				else if (points[x][y].type==2){
-					g.setColor(new Color(0.0f, 1.0f, 0.0f, 0.7f));
-				}
-				if (points[x][y].isPedestrian){
-					g.setColor(new Color(0.0f, 0.0f, 1.0f, 0.7f));
-				}
-				g.fillRect((x * size) + 1, (y * size) + 1, (size - 1), (size - 1));
-			}
-		}
+    private void drawNetting(Graphics g, int gridSpace) {
+        Insets insets = getInsets();
+        int firstX = insets.left;
+        int firstY = insets.top;
+        int lastX = this.getWidth() - insets.right;
+        int lastY = this.getHeight() - insets.bottom;
 
-	}
+        int x = firstX;
+        while (x < lastX) {
+            g.drawLine(x, firstY, x, lastY);
+            x += gridSpace;
+        }
 
-	public void mouseClicked(MouseEvent e) {
-		int x = e.getX() / size;
-		int y = e.getY() / size;
-		if ((x < points.length) && (x > 0) && (y < points[x].length) && (y > 0)) {
-			if(editType==3){
-				points[x][y].isPedestrian=true;
-			}
-			else{
-				points[x][y].type= editType;
-			}
-			this.repaint();
-		}
-	}
+        int y = firstY;
+        while (y < lastY) {
+            g.drawLine(firstX, y, lastX, y);
+            y += gridSpace;
+        }
 
-	public void componentResized(ComponentEvent e) {
-		int dlugosc = (this.getWidth() / size) + 1;
-		int wysokosc = (this.getHeight() / size) + 1;
-		initialize(dlugosc, wysokosc);
-	}
+        for (x = 1; x < points.length - 1; ++x) {
+            for (y = 1; y < points[x].length - 1; ++y) {
+                if (points[x][y].type == 0) {
+                    float staticField = points[x][y].staticField;
+                    float intensity = staticField / 100;
+                    if (intensity > 1.0) {
+                        intensity = 1.0f;
+                    }
+                    g.setColor(new Color(intensity, intensity, intensity));
+                } else if (points[x][y].type == 1) {
+                    g.setColor(new Color(1.0f, 0.0f, 0.0f, 0.7f));
+                } else if (points[x][y].type == 2) {
+                    g.setColor(new Color(0.0f, 1.0f, 0.0f, 0.7f));
+                }
+                if (points[x][y].isPedestrian) {
+                    g.setColor(new Color(0.0f, 0.0f, 1.0f, 0.7f));
+                }
+                g.fillRect((x * size) + 1, (y * size) + 1, (size - 1), (size - 1));
+            }
+        }
 
-	public void mouseDragged(MouseEvent e) {
-		int x = e.getX() / size;
-		int y = e.getY() / size;
-		if ((x < points.length) && (x > 0) && (y < points[x].length) && (y > 0)) {
-			if(editType==3){
-				points[x][y].isPedestrian=true;
-			}
-			else{
-				points[x][y].type= editType;
-			}
-			this.repaint();
-		}
-	}
+    }
 
-	public void mouseExited(MouseEvent e) {
-	}
+    public void mouseClicked(MouseEvent e) {
+        int x = e.getX() / size;
+        int y = e.getY() / size;
+        if ((x < points.length) && (x > 0) && (y < points[x].length) && (y > 0)) {
+            if (editType == 3) {
+                points[x][y].isPedestrian = true;
+            } else {
+                points[x][y].type = editType;
+            }
+            this.repaint();
+        }
+    }
 
-	public void mouseEntered(MouseEvent e) {
-	}
+    public void componentResized(ComponentEvent e) {
+        int dlugosc = (this.getWidth() / size) + 1;
+        int wysokosc = (this.getHeight() / size) + 1;
+        initialize(dlugosc, wysokosc);
+    }
 
-	public void componentShown(ComponentEvent e) {
-	}
+    public void mouseDragged(MouseEvent e) {
+        int x = e.getX() / size;
+        int y = e.getY() / size;
+        if ((x < points.length) && (x > 0) && (y < points[x].length) && (y > 0)) {
+            if (editType == 3) {
+                points[x][y].isPedestrian = true;
+            } else {
+                points[x][y].type = editType;
+            }
+            this.repaint();
+        }
+    }
 
-	public void componentMoved(ComponentEvent e) {
-	}
+    public void mouseExited(MouseEvent e) {
+    }
 
-	public void mouseReleased(MouseEvent e) {
-	}
+    public void mouseEntered(MouseEvent e) {
+    }
 
-	public void mouseMoved(MouseEvent e) {
-	}
+    public void componentShown(ComponentEvent e) {
+    }
 
-	public void componentHidden(ComponentEvent e) {
-	}
+    public void componentMoved(ComponentEvent e) {
+    }
 
-	public void mousePressed(MouseEvent e) {
-	}
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mouseMoved(MouseEvent e) {
+    }
+
+    public void componentHidden(ComponentEvent e) {
+    }
+
+    public void mousePressed(MouseEvent e) {
+    }
 
 }
--- lab5/Pedestrians/src/Point.java
+++ lab5/Pedestrians/src/Point.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Point {
 
-	public ArrayList<Point> neighbors;
+	public ArrayList<Point> neighbors = new ArrayList<>();
 	public static Integer []types ={0,1,2,3};
 	public int type;
 	public int staticField;
@@ -20,12 +20,38 @@ public class Point {
 	}
 
 	public boolean calcStaticField() {
-		return false;
+        int newPotentialField = 100001;
+        for (Point neighbour : this.neighbors) {
+            newPotentialField = Math.min(neighbour.staticField, newPotentialField);
+        }
+        if(this.staticField > newPotentialField + 1) {
+            this.staticField = newPotentialField + 1;
+            return true;
+        }
+        return false;
 	}
 	
 	public void move(){
+        if(this.isPedestrian){
+            Point nextStep = findNextStep();
+            //System.out.println("asd " + nextStep.staticField + " " + this.neighbors.get(0).staticField);
+            if(nextStep.staticField != 100000) {
+                this.isPedestrian = false;
+                this.type = 0;
+                nextStep.isPedestrian = true;
+                nextStep.type = 3;
+            }
+        }
 	}
 
+    private Point findNextStep() {
+        Point nextStep = new Point();
+        for(Point neighbour : this.neighbors) {
+            if (neighbour.staticField < nextStep.staticField && !neighbour.isPedestrian)
+                nextStep = neighbour;
+        }
+        return nextStep;
+    }
 	public void addNeighbor(Point nei) {
 		neighbors.add(nei);
 	}
